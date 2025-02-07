f8451e849c2b821dc461e80730c5ada72ec35a7a
Tester <sapdevm@gmail.com>
2025-02-07 21:18:59 +0500

    1.3.2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
+use App\Models\Gift;
+use App\Models\Notification;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Review;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Variant;
+use App\Models\Verification;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,5 +25,34 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
         ]);
+
+        Product::factory()
+            ->count(100)
+            ->has(Variant::factory()->count(3))
+            ->create();
+
+        Customer::factory()
+            ->count(100)
+            ->create();
+
+        Gift::factory()
+            ->count(50)
+            ->create();
+
+        Review::factory()
+            ->count(100)
+            ->create();
+
+        Order::factory()
+            ->count(100)
+            ->create();
+
+        Verification::factory()
+            ->count(100)
+            ->create();
+
+        Notification::factory()
+            ->count(100)
+            ->create();
     }
 }
